fe2e9a1422f14bb529244a135cfd64e583badd0d
MaorMiterani <maormiter@gmail.com>
2020-03-24 14:32:33 +0200

    Update Assignment11.py

--- Python/Assignment11.py
+++ Python/Assignment11.py
@@ -14,14 +14,11 @@ elementList1 = newlist[0::3] # Z
 elementList2 = newlist[1::3] # A
 elementList3 = newlist[2::3] # Z
 
-keyList = list(string.ascii_lowercase)
-availableKeys = keyList.copy()
-
-for i in keyList:    # I used this method on each list to check for the number of spaces
+for i in string.ascii_lowercase:    # I used this method on each list to check for the number of spaces
     numberofspaces = 0
     for char in elementList3:
         decrypted = chr(char ^ ord(i))
-        if decrypted == ' ' and i in availableKeys:
+        if decrypted == ' ':
             numberofspaces += 1
 
 solution = []
@@ -31,15 +28,15 @@ on3 = False
 
 for i in range(len(newlist)):
     if on1:
-        solution.append(chr(newlist[i] ^ ord('Z')))
+        solution.append(chr(newlist[i] ^ ord('z')))
         on1 = False
         on2 = True
     elif on2:
-        solution.append(chr(newlist[i] ^ ord('A')))
+        solution.append(chr(newlist[i] ^ ord('a')))
         on2 = False
         on3 = True
     elif on3:
-        solution.append(chr(newlist[i] ^ ord('Z')))
+        solution.append(chr(newlist[i] ^ ord('z')))
         on3 = False
         on1 = True
 
